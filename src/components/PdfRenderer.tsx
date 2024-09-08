21c5ab427587c3685396d6e209420f92f9bd2f4c
SoyaChunkz <sameermaroof26@gmail.com>
2024-09-08 22:20:45 +0530

    pdf renderer error

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,12 +1,67 @@
+'use client'
+
+import {
+  ChevronDown,
+  ChevronUp,
+  Loader2,
+  RotateCw,
+  Search,
+} from 'lucide-react'
+import { Document, Page, pdfjs } from 'react-pdf'
 
 
-const PdfRenderer = () => {
+
+import 'react-pdf/dist/Page/AnnotationLayer.css'
+import 'react-pdf/dist/Page/TextLayer.css'
+import { useToast } from '@/hooks/use-toast'
+import { useResizeDetector } from 'react-resize-detector'
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
+// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+
+interface PdfRendererProps {
+  url: string;
+}
+
+const PdfRenderer = ({ url }: PdfRendererProps) => {
+
+    const { toast } = useToast();
+
+    const { width, ref } = useResizeDetector();
+
   return (
       <div className='w-full bg-white rounded-md shadow flex flex-col items-center'>
           <div className='h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2'>
               <div className='flex items-center gap-1.5'>
                   Top Bar
               </div>
+          </div>
+
+          <div className='flex-1 w-full max-h-screen'>
+            <div ref={ref}>
+              <Document 
+                loading={
+                  <div className='flex justify-center'>
+                    <Loader2 className='my-24 h-6 w-6 animate-spin' />
+                  </div>
+                }
+                onLoadError={() => {
+                  toast({
+                    title: 'Error loading PDF',
+                    description: 'Please try again later',
+                    variant: 'destructive',
+                  })
+                }}
+                file={url} 
+                className='max-h-full'>
+                <Page
+                  width={width ? width : 1} 
+                  pageNumber={1} 
+                />
+              </Document>
+            </div>
+
           </div>
       </div>
   )
